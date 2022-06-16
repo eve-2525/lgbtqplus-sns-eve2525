1305c530f9c7e0820f9fae6e2e4d986dc836597c
eve-2525 <eve.eve.love.0625@gmail.com>
2022-06-16 21:12:21 +0900

    新規登録ビュー作成

--- app/controllers/loves_controller.rb
+++ app/controllers/loves_controller.rb
@@ -1,4 +1,5 @@
 class LovesController < ApplicationController
+  before_action :worry_params
 
   def create
     Love.create(user_id: current_user.id, worry_id: params[:id])
@@ -8,6 +9,13 @@ class LovesController < ApplicationController
   def destroy
     Love.find_by(user_id: current_user.id, worry_id: params[:id]).destroy
     redirect_to worry_path
+  end
+
+  private
+ 
+  def worry_params
+    @worry = Worry.find(params[:id])
+    @love_count = Love.where(worry_id: params[:worry_id]).count
   end
 
 end
